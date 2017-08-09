c0f68aa4ff9a1c9b126491b884ec6914cd00f479
scottfalconer <scottfalconer@gmail.com>
2017-08-09 11:46:57 -0600

    Site commit.

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+echo "Installing ACDC"
+sudo add-apt-repository ppa:ondrej/php -y
+sudo apt-get update
+
+sudo apt-get install -y php5.6-cli php5.6-curl php5.6-xml php5.6-mbstring php5.6-bz2 php5.6-gd php5.6-mysql mysql-client unzip git
+
+sudo apt-get install git composer drush
+composer global require "hirak/prestissimo:^0.3"
+
+cd site
+
+composer run-script blt-alias
+# Reinitialize the terminal.
+. ~/.bashrc
