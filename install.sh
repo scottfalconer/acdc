6467dc553ca316dca44e2afd86ec16a01c5a81b7
scottfalconer <scottfalconer@gmail.com>
2017-08-09 16:21:11 -0600

    BLT-000: Base Drupal site setup.

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ sudo apt-get install -y php5.6-cli php5.6-curl php5.6-xml php5.6-mbstring php5.6
 sudo apt-get install git composer drush
 composer global require "hirak/prestissimo:^0.3"
 
-cd site
+mysql-ctl start
 
 composer run-script blt-alias
 # Reinitialize the terminal.
